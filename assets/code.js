7f3a621fe627850538df51aa94a73ca3a2d48145
cbermudez <cbermu.97@gmail.com>
2020-03-12 16:02:00 -0400

    Process jsons data

--- assets/code.js
+++ assets/code.js
@@ -1,5 +1,11 @@
 $.getJSON('assets/olympic_games.json', (json) => {
-    console.log(json);
+    percents = []
+    years = Object.keys(json);
+    years.forEach(year => {
+        let womens = json[year]['womens'];
+        let total = json[year]['womens'] + json[year]['mens']
+        percents.push((womens/total)*100);
+    });
 });
 
 var chart = c3.generate({
